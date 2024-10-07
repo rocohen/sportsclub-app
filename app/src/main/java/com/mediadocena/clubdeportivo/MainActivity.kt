58a87fb193827732d5cd50f311e1f962f25f4373
Rosana Cohen <rocohen84@gmail.com>
2024-10-07 20:56:58 -0300

    feat:Add register activity layout

--- app/src/main/java/com/mediadocena/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/mediadocena/clubdeportivo/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.mediadocena.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
     }
+
 }

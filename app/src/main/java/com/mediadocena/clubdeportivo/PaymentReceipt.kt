5c9a79dcceb41f9393f3ebd0236e3a2be9413c3c
josefinacicchini <96507383+josefinacicchini@users.noreply.github.com>
2024-10-10 20:52:52 -0300

    export activity resolved

--- app/src/main/java/com/mediadocena/clubdeportivo/PaymentReceipt.kt
+++ app/src/main/java/com/mediadocena/clubdeportivo/PaymentReceipt.kt
@@ -5,13 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mediadocena.clubdeportivo.R.id.paymentReceiptScreen
 
 class PaymentReceipt : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_payment_receipt)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(paymentReceiptScreen)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
